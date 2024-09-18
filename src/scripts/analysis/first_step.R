6a218f767de7154cd478387fe9e0876adaa9578a
kota <85015574+tiger-sea@users.noreply.github.com>
2024-09-18 15:54:21 +0900

    modify to deal with missing values and seasonal term, and to predict specific period

--- src/scripts/analysis/first_step.R
+++ src/scripts/analysis/first_step.R
@@ -1,4 +1,4 @@
-# Baseline with horse (or another sparse prior distribution) ------------
+# Main code for estimation, prediction, and evaluation --------------------
 
 ## Import ------------------------------------------------------------------
 
@@ -30,56 +30,57 @@ date <- as.Date(df_weather$date)
 df_hrv <- left_join(df_weather, df_hrv, by = "date") %>%
             select(-c(colnames(df_weather)))
 
-### Delete unnecessary columns
+
+### Delete unnecessary columns ------------------------------------------
 df_weather <- df_weather %>%
     select(-c("date", "day_of_week", "max_gust",
               "max_wind_speed", "mean_press",
               "mean_vapor", "max_depth",
               "most_direction", "most_direction_dummy"))
 
-df_weather <- scale(df_weather)
+### Scale explanatory variable mean=0, sigma=1 --------------------------
+original_weather <- df_weather
+df_weather <- as.data.frame(scale(df_weather))
 
 ## Set list of data for stan code ---------------------------------------
 num_pred = 30 # length of prediction data
-T <- nrow(df_weather) - num_pred # length of estimation data
-y <- df_hrv$TINN # dependent variable y
-I <- sum(is.na(y))
-y[is.na(y)] <- -1
+T <- nrow(df_weather) - num_pred # length of data for estimation
+y <- scale(df_hrv$RMSSD) # dependent variable y
+I <- sum(is.na(y[1:T])) # not include missing values during prediction period
+y[is.na(y[1:T])] <- -1 # nan flag for stan (remain missing values during prediction period)
 
 data_list <- list(
     T = T, # data length without last num_pred days
     D = ncol(df_weather), # the number of features
     I = I, # the number of nan values in y
     features = as.matrix(df_weather[1:T, ]), # explanatory variable
-    y = y[1:T],
-    # i_miss = which(is.na(y)),
-    # i_obs = which(!is.na(y))
+    y = y[1:T], # y from 1 to T (period for estimation)
     T_pred = num_pred,
     features_pred = t(t(df_weather[(T+1):(T+num_pred), ]))
 )
 
-## Parameters estimation by stan MCMC -----------------------------------
+## Estimate parameters by stan MCMC -------------------------------------
 model <- stan(
     file = "./mcs_research/src/scripts/analysis/model/first_step.stan",
     data = data_list,
     seed = 1,
-    iter = 500,
-    warmup = 250,
+    iter = 4000,
+    warmup = 3000,
     chains = 4,
     # control = c(max_treedepth = 15)
 )
 
 # saveRDS(model, file = "../../model/bwp_season_HR_30.obj")
 
-## Results check --------------------------------------------------------
+## Check results --------------------------------------------------------
 mcmc_result <- rstan::extract(model)
 
-## WAIC calculation
+### WAIC ------------------------------------------------------
 log_lik <- extract_log_lik(model)
 waic(log_lik)
 # loo(log_lik)
 
-### beta (coefficients) 
+### Beta distribution (coefficients)  -----------------------------------
 beta_hist <- as.data.frame(mcmc_result$beta)
 colnames(beta_hist) <- colnames(df_weather)
 beta_hist <- melt(beta_hist)
@@ -89,74 +90,51 @@ ggplot(beta_hist, aes(x=value)) +
     geom_histogram(bins = 50) +
     facet_wrap(~beta)
 
-### organize coefficients results
+#### Organize coefficients results
 beta_result <- data.frame(colnames(df_weather),
                           apply(mcmc_result$beta, MARGIN = 2, mean))
 
 colnames(beta_result) <- c("feature", "coef_mean")
-beta_result[order(abs(beta_result$coef_mean), decreasing = TRUE),]
+beta_result[order(abs(beta_result$coef_mean), decreasing = TRUE), ]
+
+
+
+(beta_result[1,2] * sqrt(var(df_hrv$RMSSD, na.rm = T)) / 
+        sqrt(var(original_weather$min_temp, na.rm = T)))
+
 
 
 traceplot(model, pars = c("beta", "sigma_w", "sigma_y", "sigma_season"))
-stan_trace(model, inc_warmup = TRUE, pars = c("beta", "sigma_season"))
-rhat(model, pars = c("beta", "sigma_w", "sigma_y", "sigma_season"))
+# stan_trace(model, inc_warmup = TRUE, pars = c("beta", "sigma_season"))
+# rhat(model, pars = c("beta", "sigma_w", "sigma_y", "sigma_season"))
 # mcmc_rhat(rhat(model, pars = "beta"))
 
 ### Plot estimated results --------------------------------------------------
-# df_stan <- make_ci_df(mcmc_result$mu) # mu, alpha
-res <- t(apply(
-    X = mcmc_result$mu, # not for *_all, cuz useless, and for mu, alpha, pred
-    MARGIN = 2,
-    FUN = quantile,
-    probs = c(0.025, 0.5, 0.975)
-))
 
-colnames(res) <- c("lwr", "fit", "upr")
+#### Impute predicted values at y missing values
+y_filled <- y
+y_filled[y[1:T] == -1] <- apply(mcmc_result$y_mis, MARGIN = 2, mean) # impute pred of missing values
 
-y[y == -1] <- apply(mcmc_result$y_mis, 2, mean) # impute pred of missing values
+#### Make data frame of y and estimated lower/median/upper range
+df_stan <- make_ci_df(data_array = mcmc_result$pred, y = y_filled, is_pred = FALSE)
 
-df_stan <- cbind(
-    data.frame(y = y[1:nrow(res)], time=date[1:nrow(res)]),
-    as.data.frame(res)
-)
-
-plot_ssm(df_stan)
+plot_ssm(df_stan, title = "Estimation")
 
 ### Plot prediction result --------------------------------------------------
-df_stan <- make_ci_df(mcmc_result$pred) # prediction interval before y_pred
-res_pred <- t(apply(
-    X = mcmc_result$y_pred, # for y_pred
-    MARGIN = 2, # row-major (process by row)
-    FUN = quantile,
-    probs=c(0.025, 0.5, 0.975)
-))
-colnames(res_pred) <- c("lwr", "fit", "upr")
-
-# prepare data frame of predicted values
-df_pred <- cbind(
-    data.frame(y = y[(T+1):(T+num_pred)], time=date[(T+1):(T+num_pred)]),
-    as.data.frame(res_pred)
-)
-
+df_stan <- make_ci_df(mcmc_result$pred, y=y_filled, is_pred = F) # prediction interval before y_pred
+df_pred <- make_ci_df(mcmc_result$y_pred, y_filled, is_pred = T, T = T, num_pred = num_pred)
 df_all <- bind_rows(df_stan, df_pred) # combine predicted data
-
-plot_ssm(df_all)
 
 plot_pred(df_all, data_list$T_pred, T, focus=F) # plot prediction result
 
-plot(apply(mcmc_result$season, MARGIN = 2, FUN = mean), type="l")
+### Residuals ------------------------------------------------------
 
-# check residuals
 # hist(data_list$y - df_stan$fit, breaks = 100)
 hist(df_pred$fit - df_pred$y, breaks = 100)
 
 # check evaluation index
 # sqrt(sum((df_stan$fit - data_list$y) ^ 2) / data_list$T) # RMSE
-sqrt(sum((df_pred$fit - df_pred$y) ^ 2) / data_list$T_pred) # RMSE
-
-# df_hrv %>%
-#     mutate(index = row_number()) %>%  # インデックス列を追加
-#     filter(pNN50 >= 0.5)
+sqrt(sum((df_pred$fit - df_pred$y) ^ 2, na.rm = TRUE) / data_list$T_pred) # RMSE
 
 plot(df_pred$fit - df_pred$y, type="l")
 acf((df_pred$fit - df_pred$y), lag.max = 30)
--- src/scripts/analysis/model/first_step.stan
+++ src/scripts/analysis/model/first_step.stan
@@ -1,3 +1,6 @@
+// This code could be main stan code.
+// This code is available with data including missing values in dependent variable y.
+// But, maybe not allowed to use data including missing values in explanatory variable x.
 data {
     int T; // length of features
     int D; // the number of features
@@ -15,19 +18,29 @@ parameters {
     real<lower=0> sigma_w; // process noise standard deviation
     real<lower=0> sigma_y; // observation noise standard deviation
 
+    // estimation missing values in y
+    vector<lower=0>[I] y_mis;
+
+    // seasonality
+    vector[T] season;
+    real<lower=0> sigma_season;
+
     // prior distribution params
     vector[D] beta; // partial regression coefficients
     real<lower=0> tau; // parameter of prior distribution for beta
-
-    // estimation missing values in y
-    vector[I] y_mis;
 }
 
 transformed parameters {
+    // mu (state) included trend and seasonality
+    vector[T] mu_with_component;
+    for(t in 1:T) {
+        mu_with_component[t] = mu[t] + season[t];
+    }
+    
     // regression model
-    vector[T] alpha;
-    for (t in 1:T) {
-        alpha[t] = mu[t] + dot_product(features[t, ], beta);
+    vector<lower=0>[T] alpha;
+    for(t in 1:T) {
+        alpha[t] = mu_with_component[t] + dot_product(features[t, ], beta);
     }
 }
 
@@ -37,10 +50,14 @@ model {
         beta[d] ~ double_exponential(0, tau);
     }
 
-    // state equation
-    mu[3:T] ~ normal(2 * mu[2:T-1] - mu[1:T-2], sigma_w);
+    for(t in 7:T) {	
+        season[t] ~ normal(-sum(season[t-6:t-1]), sigma_season);
+    }
 
-    // observation equation (for both observed and imputed values)
+    // state equation
+    mu[3:T] ~ normal(2*mu[2:T-1]-mu[1:T-2], sigma_w);
+
+    // observation equation (for both observed and missing values)
     int miss = 0;
     for(t in 1:T) {
         if(y[t] != -1) {
@@ -49,5 +66,44 @@ model {
             miss = miss + 1;
             y_mis[miss] ~ normal(alpha[t], sigma_y);
         }
+    }
+}
+
+generated quantities {
+    vector[T] log_lik; // for WAIC calculation
+    for(t in 1:T) {
+        log_lik[t] = normal_lpdf(y[t] | alpha[t], sigma_y);
+    }
+
+    // prediction part (variables named *_all include prediction period)
+    // seasonality
+    vector[T+T_pred] season_all;
+    season_all[1:T] = season; // same values within T
+    for(t in 1:T_pred) {
+        season_all[T+t] = normal_rng(-sum(season_all[(T+t-7):(T+t-1)]), sigma_season);
+    }
+
+    vector[T+T_pred] mu_all;
+    vector[T+T_pred] mu_with_component_all; // mu + seasonality
+    vector[T+T_pred] alpha_all; // mu + seasonality and regression
+    vector[T_pred] y_pred;
+    mu_all[1:T] = mu; // same values within T
+    mu_with_component_all[1:T] = mu_with_component; // same value within T
+    alpha_all[1:T] = alpha; // same values within T
+    for(t in 1:T_pred) {
+        mu_all[T+t] = normal_rng(2*mu_all[T+t-1]-mu_all[T+t-2], sigma_w);
+        mu_with_component_all[T+t] = mu_all[T+t] + season_all[T+t];
+
+        // calculate alpha at time T + t
+        alpha_all[T+t] = mu_with_component_all[T+t] + dot_product(features_pred[t, ], beta);
+
+        // predict y at time T + t using the predicted alpha
+        y_pred[t] = normal_rng(alpha_all[T+t], sigma_y);
+    }
+    
+    // just in case
+    vector[T] pred;
+    for(t in 1:T) {
+        pred[t] = normal_rng(alpha[t], sigma_y);
     }
 }
